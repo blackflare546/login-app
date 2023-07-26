0da27cbf2cb64c759843f35f03dea04bf8f34b55
Glenn Mark <gflores@hyperstacksinc.com>
2023-07-26 11:16:47 +0800

    feat: use react hook form on Login

--- src/scenes/Entry/screens/login.screen.tsx
+++ src/scenes/Entry/screens/login.screen.tsx
@@ -1,62 +1,55 @@
 import React from "react";
 import {
-  View,
-  Text,
-  TextInput,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
+  LoginContainer,
+  EmailInput,
+  PassInput,
+  LoginButton,
+  ButtonText,
+} from "../styles/login.styled";
+import { useForm, Controller } from "react-hook-form";
+import { Alert, Text } from "react-native";
 
 export default () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data: object) => console.log(data);
+
   return (
-    <View style={styles.Container}>
-      <Text style={styles.LoginText}>Login</Text>
-      <TextInput style={styles.Input} placeholder="Email" />
-      <TextInput style={styles.Input} placeholder="Password" />
-      <TouchableOpacity style={styles.LoginButton}>
-        <Text style={styles.ButtonText}>Login</Text>
-      </TouchableOpacity>
-      <Text style={styles.ForgotPassword}>Forgot your password?</Text>
-    </View>
+    <LoginContainer>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <EmailInput onBlur={onBlur} onChangeText={onChange} value={value} />
+        )}
+        name="email"
+      />
+      {errors.email && Alert.alert("Login Error", "Email is Required")}
+
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <PassInput onBlur={onBlur} onChangeText={onChange} value={value} />
+        )}
+        name="password"
+      />
+
+      <LoginButton onPress={handleSubmit(onSubmit)}>
+        <ButtonText>Submit</ButtonText>
+      </LoginButton>
+    </LoginContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  Container: {
-    flex: 1,
-    alignItems: "center",
-    paddingHorizontal: 20,
-    gap: 20,
-  },
-  LoginText: {
-    fontSize: 30,
-    fontWeight: "600",
-    marginVertical: 40,
-  },
-  Input: {
-    backgroundColor: "#fff",
-    height: 50,
-    width: "100%",
-    borderRadius: 10,
-    padding: 20,
-  },
-  LoginButton: {
-    marginTop: 25,
-    backgroundColor: "#5DB075",
-    height: 60,
-    width: "100%",
-    borderRadius: 40,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  ButtonText: {
-    fontSize: 30,
-    fontWeight: "400",
-    color: "#fff",
-  },
-  ForgotPassword: {
-    color: "#5DB075",
-    fontSize: 18,
-    fontWeight: "500",
-  },
-});
---
+++ src/scenes/Entry/styles/login.styled.ts
@@ -0,0 +1,58 @@
+import { TextInput } from 'react-native';
+import styled from 'styled-components/native';
+
+export const LoginContainer = styled.View`
+    flex: 1;
+    align-items: center;
+    padding-horizontal: 20px;
+    margin-top: 100px;
+    gap: 20px;
+`;
+
+export const LoginText = styled.Text`
+    font-size: 30px;
+    font-weight: 600;
+    margin-vertical: 40px;
+`;
+
+export const EmailInput = styled(TextInput).attrs(
+    {placeholder: "Email"}
+)`
+    backgroundColor: #fff;
+    height: 50px;
+    width: 100%;
+    border-radius: 10px;
+    padding: 20px
+`
+
+export const PassInput = styled(TextInput).attrs({
+    placeholder: "Password",
+    secureTextEntry: true
+})`
+    backgroundColor: #fff;
+    height: 50px;
+    width: 100%;
+    border-radius: 10px;
+    padding: 20px;
+`
+export const LoginButton = styled.TouchableOpacity`
+    margin-top: 25px;
+    background-color: #5DB075;
+    height: 60px;
+    width: 100%;
+    border-radius: 40px;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ButtonText = styled.Text`
+    font-size: 24px;
+    font-weight: 400;
+    color: #fff;
+`
+
+export const ForgotPassword = styled.Text`
+    color: #5DB075;
+    font-size: 18px;
+    font-weight: 500;
+`
